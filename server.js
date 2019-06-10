bdbf3acecba564ecb15c166471a697e01a6b7bd5
jslgmy <49324049+jslgmy@users.noreply.github.com>
2019-06-09 22:41:27 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+﻿var path = require('path');
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = 3000;
+
+var moviesData = require('./moviesData');
+
+
+app.use(bodyParser.json());
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+app.listen(port, function () {
+  console.log('server is listening on port', port);
+});
+
+app.get('/', function (req, res) {
+    res.status(200).render('home', {
+        movies: moviesData
+    });
+});
+
+
+
+
