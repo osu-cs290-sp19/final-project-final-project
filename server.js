2c52f9fcd7ef1a9e0ab2fad5476e1f94b00231fb
jslgmy <49324049+jslgmy@users.noreply.github.com>
2019-06-13 08:47:50 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,27 +1,53 @@
-﻿var path = require('path');
-var express = require('express');
-var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
+var http = require('http');
+var fs = require('fs');
 
-var app = express();
-var port = 3000;
+var css = fs.readFileSync('./public/style.css');
+var html = fs.readFileSync('./public/index.html');
+var pg404 = fs.readFileSync('./public/404.html');
+var js = fs.readFileSync('./public/index.js');
 
-var moviesData = require('./moviesData');
+function requestHandler (req, res) {
 
+  console.log("req.method", req.method);
+  console.log("req.url", req.url);
+  console.log("req.headers", req.headers);
 
-app.use(bodyParser.json());
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
-app.set('view engine', 'handlebars');
-app.listen(port, function () {
-  console.log('server is listening on port', port);
+    if ((req.url == '/index.html') || (req.url == '/')) {
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(html);
+
+    }
+
+    else if (req.url == '/index.js') {
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/js');
+        res.write(js);
+
+    }
+
+    else if (req.url == '/style.css') {
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/css');
+        res.write(css);
+
+    }
+
+    else {
+
+        res.statusCode = 404;
+        res.setHeader('Content-Type', 'text/html');
+        res.write(pg404);
+
+    }
+
+    res.end();
+}
+
+var server = http.createServer(requestHandler);
+server.listen(3000, function () {
+    console.log("Server running on port 3000");
 });
-
-app.get('/', function (req, res) {
-    res.status(200).render('home', {
-        movies: moviesData
-    });
-});
-
-
-
-
