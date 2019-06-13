32d09aafca291eea2e59bd91b893d77dd91669ab
jslgmy <49324049+jslgmy@users.noreply.github.com>
2019-06-13 09:01:10 -0700

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,9 +1,3 @@
-/*
- * Add the contents of your index.js file from Assignment 3 here to see the
- * interactions you implemented.  This is not required for your grade on this
- * assignment, but it'll allow you to have the full experience of the site
- * as we've implemented it so far.
-*/
 
 var modal = document.getElementById("create-movie-modal");
 var modalBD = document.getElementById("modal-backdrop");
